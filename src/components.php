1f63a77ffd1d18ed4f74a5d54e61e01ebc946f11
ownzaidi <ownzaidi@gmail.com>
2021-09-21 19:47:50 +0500

    Task Code - 14

--- src/components.php
+++ src/components.php
@@ -5,5 +5,5 @@ use Snowdog\DevTest\Component\RouteRepository;
 use Ownzaidi\Task6\Controller\ImportSiteMap;
 use Ownzaidi\Task6\Command\ImportSiteMapCmd;
 
-RouteRepository::registerRoute('POST', '/importwebsite', ImportSiteMap::class, 'execute');
+RouteRepository::registerRoute('POST', '/importsitemap', ImportSiteMap::class, 'execute');
 CommandRepository::registerCommand('import_sitemap', ImportSiteMapCmd::class);
